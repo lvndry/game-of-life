ed09ab499d539eb705240c0d31965d5528281e22
lvndry <monga.4landry@gmail.com>
2020-03-20 18:08:18 +0100

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -55,26 +55,6 @@ impl Universe {
         (row * self.width + column) as usize
     }
 
-    fn live_neighbours_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-
-        for delta_row in [self.width - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.height - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbour_row = (row + delta_row) % self.height;
-                let neighbour_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbour_row, neighbour_col);
-
-                count += self.cells[idx] as u8;
-            }
-        }
-
-        count
-    }
-
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -103,12 +83,57 @@ impl Universe {
         self.width
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect()
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..height * self.width).map(|_i| Cell::Dead).collect()
+    }
+
     pub fn height(&self) -> u32 {
         self.height
     }
 
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
+    fn live_neighbours_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [self.width - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.height - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbour_row = (row + delta_row) % self.height;
+                let neighbour_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbour_row, neighbour_col);
+
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
     }
 }
 
