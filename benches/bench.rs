265538de53dd16e0211081e0bbd207c92d3220a8
lvndry <monga.4landry@gmail.com>
2020-03-21 09:08:50 +0100

    improved performanes by 8

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,7 +6,6 @@ extern crate test;
 #[bench]
 fn universe_ticks(b: &mut test::Bencher) {
     let mut universe = game_of_life::Universe::new();
-
     b.iter(|| {
         universe.tick();
     });
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// #[wasm_bindgen]
+#[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
@@ -26,14 +26,14 @@ impl Cell {
     }
 }
 
-// #[wasm_bindgen]
+#[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
     cells: Vec<Cell>,
 }
 
-// #[wasm_bindgen]
+#[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
         let width = 120;
@@ -65,31 +65,30 @@ impl Universe {
     }
 
     pub fn tick(&mut self) {
-        let _timer = Timer::new("Universe::tick");
+        // let _timer = Timer::new("Universe::tick");
         let mut next = {
-            let _timer = Timer::new("Allocate next cells");
+            // let _timer = Timer::new("allocate next cells");
             self.cells.clone()
         };
-
-        let _timer = Timer::new("New generation");
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbours = self.live_neighbours_count(row, col);
-
-                let next_cell = match (cell, live_neighbours) {
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    (Cell::Dead, 3) => Cell::Alive,
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
+        {
+            // let _timer = Timer::new("new generation");
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    let idx = self.get_index(row, col);
+                    let cell = self.cells[idx];
+                    let live_neighbors = self.live_neighbor_count(row, col);
+                    let next_cell = match (cell, live_neighbors) {
+                        (Cell::Alive, x) if x < 2 => Cell::Dead,
+                        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                        (Cell::Alive, x) if x > 3 => Cell::Dead,
+                        (Cell::Dead, 3) => Cell::Alive,
+                        (otherwise, _) => otherwise,
+                    };
+                    next[idx] = next_cell;
+                }
             }
         }
-        let _timer = Timer::new("Free old cells");
+        // let _timer = Timer::new("free old cells");
         self.cells = next;
     }
 
@@ -135,22 +134,48 @@ impl Universe {
         }
     }
 
-    fn live_neighbours_count(&self, row: u32, column: u32) -> u8 {
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
 
-        for delta_row in [self.width - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.height - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
+        let north = if row == 0 { self.height - 1 } else { row - 1 };
 
-                let neighbour_row = (row + delta_row) % self.height;
-                let neighbour_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbour_row, neighbour_col);
+        let south = if row == self.height - 1 { 0 } else { row + 1 };
 
-                count += self.cells[idx] as u8;
-            }
-        }
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
 
         count
     }
